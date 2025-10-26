00c468f503519b5df9ea25d32a83e207aaef058e
Sweta Parekh <parekhs3@montclair.edu>
2025-10-26 19:25:45 -0400

    Initial Spring Boot app for midterm

---
+++ src/main/java/com/example/midterm/MidtermAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.midterm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidtermAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MidtermAppApplication.class, args);
+	}
+
+}
